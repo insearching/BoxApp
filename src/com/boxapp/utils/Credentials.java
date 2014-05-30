ceb110ad5440b19730b63e155c7605affbdb6f5b
Serhii Hrabas <serhii@sjinnovation.com>
2014-05-30 15:42:27 +0300

    added keymaps class

---
+++ src/com/boxapp/utils/Credentials.java
@@ -0,0 +1,15 @@
+package com.boxapp.utils;
+
+/**
+ * Created by insearching on 30.05.2014.
+ */
+public class Credentials {
+    //Request URLs
+    public static final String AUTH_URL = "https://api.box.com/oauth2/authorize";
+    public static final String SERVICE_REQUEST_URL = "https://api.box.com/oauth2/token";
+    public static final String ROOT_URL = "https://api.box.com/2.0/";
+
+    //Application info on service
+    public static final String CLIENT_ID = "8js646cfoogrnoi6zvnnb2yfl4f3uuok";
+    public static final String CLIENT_SECRET = "KXMvD9FyBLCtsnHVtY27lBIdPyBLGjMK";
+}
---
+++ src/com/boxapp/utils/KeyMap.java
@@ -0,0 +1,43 @@
+package com.boxapp.utils;
+
+/**
+ * Created by insearching on 30.05.2014.
+ */
+public class KeyMap {
+    public static final String REQUEST_URL = "request_url";
+    public static final String ACCESS_TOKEN = "access_token";
+    public static final String FILE_IDENT = "file_ident";
+    public static final String FILE_NAME = "file_name";
+    public static final String POSITION = "position";
+
+    public static final String CLIENT_ID = "client_id";
+    public static final String CLIENT_SECRET = "client_secret";
+    public static final String GRANT_TYPE = "grant_type";
+    public static final String REFRESH_TOKEN = "refresh_token";
+
+
+    //Filetypes
+    public static final String JPG = ".jpg";
+    public static final String JPEG = ".jpeg";
+    public static final String DOC = ".doc";
+    public static final String DOCX = ".docx";
+    public static final String PNG = ".png";
+    public static final String PDF = ".pdf";
+    public static final String TXT = ".txt";
+
+
+    //Cache files
+    public static final String USER_DETAILS = "userdetails";
+    public static final String DOWNLOADED_FILES = "downloaded_files";
+
+    //JSON
+    public static final String ITEM_COLLECTION = "item_collection";
+    public static final String ENTRIES = "entries";
+    public static final String NAME = "name";
+    public static final String TYPE = "type";
+    public static final String ID = "id";
+    public static final String FOLDER = "folder";
+    public static final String FILE = "file";
+    public static final String FILE_ID = "file_id";
+    public static final String ETAG = "etag";
+}
